fdf0cb266574f003a5abdc07e51e6d0977253c4f
Sparsh-Kumar <sparshkumar14998@gmail.com>
2025-02-06 21:25:27 +0530

    chat widget.

--- src/app/chatwidget/chatwidget.component.ts
+++ src/app/chatwidget/chatwidget.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-chatwidget',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './chatwidget.component.html',
   styleUrl: './chatwidget.component.css'
 })
 export class ChatwidgetComponent {
+  public isChatBoxVisible: boolean = false;
+  constructor() {
+  }
 
+  public toggleWidget() {
+    this.isChatBoxVisible = !this.isChatBoxVisible;
+  }
 }
